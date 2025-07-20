6a1ecedf81b964a63f31232d0503a56bf639bedf
Ma1n0s <kirill000e@gmail.com>
2025-07-20 20:32:31 +0200

    page+

---
+++ nuxt-app/pages/page1.vue
@@ -0,0 +1,12 @@
+<script setup="ts">
+import Slider from '~/components/Slider/Slider.vue';
+import Accordion from '~/components/ui/Accordion/Accordion.vue';
+import Block from '~/components/ui/Accordion/Block.vue';
+</script>
+<template>
+    <div class="min-h-screen bg-tech-light">
+        <Slider />
+        <Block />
+        <Accordion />
+    </div>
+</template>
