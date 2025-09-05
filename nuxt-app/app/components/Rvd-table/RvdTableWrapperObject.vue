d46a19074d7737a263565b1e3ad5cadd1dacb395
Ma1n0s <kirill000e@gmail.com>
2025-09-05 10:24:40 +0500

    другие табл

---
+++ nuxt-app/app/components/Rvd-table/RvdTableWrapperObject.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  tableDataObject: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <section class="w-full py-4 sm:py-6 md:py-8 px-2 sm:px-4 lg:px-6 lg:max-w-7xl lg:mx-auto">
+    <div class="hidden lg:block">
+      <RvsTableDesktopObject :tableData="tableDataObject" />
+    </div>
+    <div class="lg:hidden space-y-4">
+      <RvdTableMobile :tableData="tableDataObject" />
+    </div>
+  </section>
+</template>
