e52bb3a72dd85e8a86912066779a1d89a9d6946e
Nnder <asfahaikrkt6k56@yandex.ru>
2025-09-15 12:13:23 +0300

    откат

--- nuxt-app/app/pages/Sell/sell-komplektushie-rvd.vue
+++ nuxt-app/app/pages/Sell/sell-komplektushie-rvd.vue
@@ -10,7 +10,7 @@ import PartnerBlock from '~/components/Page/PartnerBlock.vue'
 import Slide from '~/components/Slider/Slide.vue'
 
 definePageMeta({
-  path: '/sell-komplektushie-rvd',
+  path: '/sell-komplektuyushie-rvd',
 })
 
 useHead({
