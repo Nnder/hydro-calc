b5e0752dce9782f0b3971e6643c5843bd5f8f06d
Nnder <asfahaikrkt6k56@yandex.ru>
2025-09-29 21:42:59 +0300

    loadFunc применение

--- nuxt-app/app/components/Block/InformationBlock.vue
+++ nuxt-app/app/components/Block/InformationBlock.vue
@@ -10,10 +10,10 @@ defineProps({
     type: Object,
     required: true,
   },
-  position:{
+  position: {
     type: String,
-    default: 'right'
-  }
+    default: 'right',
+  },
 })
 </script>
 
@@ -21,10 +21,10 @@ defineProps({
   <section class="w-full h-[50vh] min-h-[400px]">
     <div class="w-full h-full bg-hydro-power">
       <div class="max-w-7xl h-full mx-auto px-4 sm:px-6 lg:px-8 flex items-center">
-        <div class="flex flex-col  items-center justify-between w-full gap-8"
-        :class="[
-          position === 'right' ? 'md:flex-row' : 'md:flex-row-reverse'
-        ]">
+        <div
+          class="flex flex-col items-center justify-between w-full gap-8"
+          :class="[position === 'right' ? 'md:flex-row' : 'md:flex-row-reverse']"
+        >
           <div class="text-white max-w-2xl">
             <h2 class="text-2xl md:text-4xl font-bold mb-4 leading-tight">
               {{ blockData.title }}
@@ -53,6 +53,7 @@ defineProps({
                 :modelPath="blockData.modelSrc"
                 :canvasColor="blockData.modelBgColor"
                 :screenIncrease="blockData.scale || 0.5"
+                :loadFunc="blockData?.loadFunc"
                 class="!h-[400px]"
               />
 
--- nuxt-app/app/pages/index.vue
+++ nuxt-app/app/pages/index.vue
@@ -76,20 +76,20 @@ const blockDataVariantTwo = {
   type: '3d',
   modelSrc: '/3d/Сборка ковша.glb',
   modelBgColor: '#2563EB',
-  scale: 0.3
+  scale: 0.3,
+  loadFunc: model => {
+    model.rotation.x = Math.PI / 2
+    model.rotation.y = Math.PI
+  },
 }
-
-
-
-
 </script>
 
 <template>
   <Slider />
   <ServiceBlock data-aos="fade-up" />
-  <InformationBlock :blockData="blockData" data-aos="fade-up" class=""/>
-  <InformationBlockLeft :blockData="blockDataVariant" position="left" data-aos="fade-up" class=""/>
-  <InformationBlock :blockData="blockDataVariantTwo" data-aos="fade-up" class=""/>
+  <InformationBlock :blockData="blockData" data-aos="fade-up" />
+  <InformationBlockLeft :blockData="blockDataVariant" position="left" data-aos="fade-up" />
+  <InformationBlock :blockData="blockDataVariantTwo" data-aos="fade-up" />
   <Block data-aos="fade-up" />
   <Advantages />
   <Contact data-aos="fade-up" />
