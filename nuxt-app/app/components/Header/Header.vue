8c831254fe39eef6ddfa3dc16d1eedaf028b36b5
Ma1n0s <kirill000e@gmail.com>
2025-09-30 11:46:17 +0500

    шапка доб подпункт продажа

--- nuxt-app/app/components/Header/Header.vue
+++ nuxt-app/app/components/Header/Header.vue
@@ -13,12 +13,18 @@ const mobileSubMenuState = reactive({
   services: false,
   repair: false,
   hydraulics: false,
+  sell: false,
 })
 
 const mainMenu = [
   {
     id: 'services',
     name: 'Услуги',
+    hasSubmenu: true,
+  },
+  {
+    id: 'sell',
+    name: 'Продажа',
     hasSubmenu: true,
   },
   {
@@ -107,6 +113,37 @@ const servicesSubMenu = [
       { name: 'Фильтров и фильтроэлементов', link: '/sell-filtrov' },
       { name: 'Комплектующие РВД', link: '/sell-komplektushie-rvd' },
       { name: 'ГСМ и технических жидкостей', link: '/sell-shidkostey' },
+    ],
+  },
+]
+
+const sellSubMenu = [
+  {
+    id: 'hydraulics',
+    name: 'Гидравлика',
+    items: [
+      { name: 'Гидронасосы', link: '/sell-gidronasosov' },
+      { name: 'Гидроцилиндры', link: '/sell-gidrocilindrov' },
+      { name: 'Гидромоторы', link: '/sell-gidromotorov' },
+      { name: 'Распределители', link: '/sell-gidroraspredelitelei' },
+      { name: 'Клапаны давления', link: '/sell-klapanov-davleniya' },
+    ],
+  },
+  {
+    id: 'components',
+    name: 'Комплектующие',
+    items: [
+      { name: 'Уплотнения', link: '/sell-uplotnenie' },
+      { name: 'Фильтры и фильтроэлементы', link: '/sell-filtrov' },
+      { name: 'Комплектующие РВД', link: '/sell-komplektushie-rvd' },
+    ],
+  },
+  {
+    id: 'fluids',
+    name: 'ГСМ и жидкости',
+    items: [
+      { name: 'Масла и смазки', link: '/sell-masla' },
+      { name: 'Технические жидкости', link: '/sell-shidkostey' },
     ],
   },
 ]
@@ -374,6 +411,49 @@ const { open } = useModal()
                         </transition>
                       </li>
                     </template>
+                  </ul>
+                </div>
+              </template>
+            </div>
+          </div>
+        </div>
+      </transition>
+
+      <transition
+        enter-active-class="transition-all duration-300 ease-out"
+        enter-from-class="opacity-0 -translate-y-2 z-40"
+        enter-to-class="opacity-100 translate-y-0 z-50"
+        leave-active-class="transition-all duration-200 ease-in"
+        leave-from-class="opacity-100 translate-y-0 z-50"
+        leave-to-class="opacity-0 -translate-y-2 z-40"
+      >
+        <div
+          v-show="activeMenu === 'sell'"
+          class="absolute left-0 right-0 top-full bg-white shadow-xl py-6"
+          :class="activeMenu === 'sell' ? 'z-50' : 'z-40'"
+          @mouseenter="openSubMenu('sell')"
+          @mouseleave="closeSubMenu"
+        >
+          <div class="container mx-auto px-4">
+            <div class="flex flex-wrap gap-8">
+              <template v-for="category in sellSubMenu" :key="category.id">
+                <div class="flex-1 min-w-[250px]">
+                  <h3 class="text-lg font-bold mb-4 text-tech-dark border-b border-hydro-power pb-2">
+                    {{ category.name }}
+                  </h3>
+                  <ul class="space-y-3">
+                    <li v-for="item in category.items" :key="item.name">
+                      <NuxtLink
+                        :to="item.link"
+                        class="text-hydro-steel hover:text-hydro-power flex items-center text-base transition-colors duration-200"
+                      >
+                        <Icon
+                          name="material-symbols:arrow-forward-ios-rounded"
+                          class="min-w-4 min-h-4 mr-2 text-hydro-power transition-transform duration-200 group-hover:translate-x-0.5"
+                        />
+                        {{ item.name }}
+                      </NuxtLink>
+                    </li>
                   </ul>
                 </div>
               </template>
