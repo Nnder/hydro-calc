ac05f9f57e35ed97c5f1c107b3917e349ebfd4cb
nnder <asfahaikrkt6k56@yandex.ru>
2025-09-30 12:34:40 +0500

    - комментарии

--- nuxt-app/app/components/RepairPartsSelector/RepairPartsSelector.vue
+++ nuxt-app/app/components/RepairPartsSelector/RepairPartsSelector.vue
@@ -2,7 +2,6 @@
   <div class="bg-tech-light" data-aos="fade-up" data-aos-delay="200">
     <div class="w-4/5 mx-auto py-8 md:py-6 px-4 sm:px-3 lg:px-4 rounded-2xl mt-8 mb-4">
       <div class="flex items-center">
-        <!-- Левая секция (список деталей) -->
         <section class="flex-1 p-4 h-fit bg-white rounded-2xl shadow-lg overflow-auto">
           <div class="flex flex-col md:flex-row justify-between items-start md:items-center mb-6 gap-3">
             <div class="space-y-1">
@@ -108,7 +107,6 @@
           </div>
         </section>
 
-        <!-- Правая секция (изображение) -->
         <section class="flex-1 relative">
           <div class="w-full h-full rounded-2xl overflow-hidden relative flex items-start justify-center bg-gray-100 ">
             <div class="relative" :style="imageStyle">
