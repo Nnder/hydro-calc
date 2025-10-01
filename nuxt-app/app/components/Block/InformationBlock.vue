34aa2dd86755e419089ed239e51396837bc6d1cb
nnder <asfahaikrkt6k56@yandex.ru>
2025-10-01 12:10:37 +0500

    more mobile

--- nuxt-app/app/components/Block/InformationBlock.vue
+++ nuxt-app/app/components/Block/InformationBlock.vue
@@ -17,15 +17,15 @@ defineProps({
 </script>
 
 <template>
-  <section class="w-full h-[50vh] min-h-[400px]">
+  <section class="w-full h-full min-h-[300px]">
     <div class="w-full h-full bg-hydro-power">
       <div class="max-w-7xl h-full mx-auto px-4 sm:px-6 lg:px-8 flex items-center">
         <div
-          class="flex flex-col items-center justify-between w-full gap-8"
-          :class="[position === 'right' ? 'md:flex-row' : 'md:flex-row-reverse']"
+          class=" flex flex-col items-start lg:items-center justify-between w-full gap-8"
+          :class="[position === 'right' ? 'lg:flex-row' : 'lg:flex-row-reverse']"
         >
-          <div class="text-white max-w-2xl">
-            <h2 class="text-2xl md:text-4xl font-bold mb-4 leading-tight">
+          <div class="mt-8 lg:mt-0 text-white xl:max-w-2xl">
+            <h2 class="text-2xl xl:text-4xl font-bold mb-4 leading-tight">
               {{ blockData.title }}
             </h2>
             <p class="text-lg mb-6 opacity-90">
