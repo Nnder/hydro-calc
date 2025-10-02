e101d7002f74f3a16729b1fe5799aaae5bb40dcc
nnder <asfahaikrkt6k56@yandex.ru>
2025-10-02 14:28:07 +0500

    bug fix

--- nuxt-app/app/components/RepairPartsSelector/RepairPartsSelector.vue
+++ nuxt-app/app/components/RepairPartsSelector/RepairPartsSelector.vue
@@ -73,7 +73,7 @@
                 leave-from-class="opacity-100 max-h-96"
                 leave-to-class="opacity-0 max-h-0"
               >
-                <div v-if="part.show && (part.description || part.features.length)" class="overflow-visible">
+                <div v-if="part.show && (part.description || part?.features?.length)" class="overflow-visible">
                   <div
                     class="relative mt-3 p-4 sm:p-5 bg-gradient-to-br from-gray-50 to-white rounded-xl shadow-sm"
                   >
