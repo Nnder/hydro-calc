b3a1c1a269f39d5d466ea264445748d3e80bcdb9
nnder <asfahaikrkt6k56@yandex.ru>
2025-10-02 13:06:04 +0500

    + пробелы

--- nuxt-app/app/components/Accessories/AccessoriesGrid.vue
+++ nuxt-app/app/components/Accessories/AccessoriesGrid.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <section class="max-w-8xl flex items-center py-8 md:py-16">
+  <section class="max-w-8xl flex items-center py-8 md:py-16 px-4">
     <!-- <div class="text-center mb-8 md:mb-12">
       <h2 class="text-2xl md:text-4xl font-bold text-hydro-power">Дополнительные комплектующие</h2>
       <p class="mt-2 text-sm md:text-base">Всегда в наличии расходные материалы для РВД</p>
