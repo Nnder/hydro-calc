33207ba013ebd3fa12d7d95dcbea32a04d94ca0d
nnder <asfahaikrkt6k56@yandex.ru>
2025-10-20 04:09:37 +0500

    заголовок

--- nuxt-app/app/components/Accessories/AccessoriesGrid.vue
+++ nuxt-app/app/components/Accessories/AccessoriesGrid.vue
@@ -4,20 +4,19 @@ defineProps({
     type: Array,
     required: true,
   },
+  title: {
+    type: String,
+  },
 })
 </script>
 
 <template>
-  <section class="max-w-8xl flex items-center py-8 md:py-16 px-4">
-    <!-- <div class="text-center mb-8 md:mb-12">
-      <h2 class="text-2xl md:text-4xl font-bold text-hydro-power">Дополнительные комплектующие</h2>
-      <p class="mt-2 text-sm md:text-base">Всегда в наличии расходные материалы для РВД</p>
-      grid grid-cols-4 gap-4 md:gap-6 justify-center
-    </div> -->
+  <section class="max-w-8xl flex flex-col items-center py-8 md:py-16 px-4">
+    <div class="text-center mb-8 md:mb-12" v-if="title">
+      <h2 class="text-2xl md:text-4xl font-bold text-hydro-power">{{ title }}</h2>
+    </div>
 
-    <div 
-      class="flex flex-1 flex-wrap justify-center gap-6"
-    >
+    <div class="flex flex-1 flex-wrap justify-center gap-6">
       <AccessoriesItem
         v-for="(item, index) in items"
         :key="index"
