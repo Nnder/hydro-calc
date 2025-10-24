bdd7765c68545135234e1db3532ef1a42d5a616f
nnder <asfahaikrkt6k56@yandex.ru>
2025-10-24 08:57:26 +0500

    + картики

--- nuxt-app/app/pages/sell.vue
+++ nuxt-app/app/pages/sell.vue
@@ -59,13 +59,13 @@ const categories = ref([
       },
       {
         title: 'Электростанции',
-        image: 'https://ammoxx.ru/upload/medialibrary/577/57778c5c2b33268e2910c96f080beb6c.jpg',
+        image: 'https://tss.ru/upload/iblock/7fc/xilvh1zxj50p1434qffaquamxsbauann.jpg',
         description: 'Масла и жидкости для гидравлических систем',
         link: '/sell/category/electrostancii',
       },
       {
         title: 'Строительное оборудование',
-        image: 'https://ammoxx.ru/upload/medialibrary/577/57778c5c2b33268e2910c96f080beb6c.jpg',
+        image: 'https://tss.ru/upload/iblock/69c/0t7ppxa89e0ey7eb5h1r3h1i6r8hjsqa.jpg',
         description: 'Масла и жидкости для гидравлических систем',
         link: '/sell/category/stroitelnoe-oborydovanie',
       },
