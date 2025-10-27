2345bf2dc05b8fc077e4eec14897894dbed5c60e
nnder <asfahaikrkt6k56@yandex.ru>
2025-10-27 20:39:44 +0500

    вынес мусор

--- nuxt-app/app/app.vue
+++ nuxt-app/app/app.vue
@@ -106,28 +106,4 @@ useSchemaOrg([
     name: 'Nuxt SEO',
   }),
 ])
-
-const {
-  cookiesEnabled,
-  cookiesEnabledIds,
-  isConsentGiven,
-  isModalActive,
-  moduleOptions,
-} = useCookieControl()
-
-// example: react to a cookie being accepted
-watch(
-  () => cookiesEnabledIds.value,
-  (current, previous) => {
-    if (
-      !previous?.includes('google-analytics') &&
-      current?.includes('google-analytics')
-    ) {
-      // cookie with id `google-analytics` got added
-      window.location.reload() // placeholder for your custom change handler
-    }
-  },
-  { deep: true },
-)
-
 </script>
--- nuxt-app/app/plugins/analytics.client.ts
+++
@@ -1,8 +0,0 @@
-//  plugins/analytics.client.ts
-
-// example: initialization based on enabled cookies
-const cookieControl = useCookieControl()
-
-if (cookieControl.cookiesEnabledIds.value.includes('google-analytics')) {
-  initGoogleAnalytics()
-}
