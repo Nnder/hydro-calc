43c8e71898fb55fdb1ad9e03ed03dc33c0a0955b
nnder <asfahaikrkt6k56@yandex.ru>
2025-11-05 10:58:23 +0500

    добавил метрику через плагин

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -23,8 +23,20 @@ export default defineNuxtConfig({
     '@nuxtjs/google-fonts',
     'nuxt-schema-org',
     'nuxt-aos',
+    'nuxt-yandex-metrika',
     // '@dargmuesli/nuxt-cookie-control',
   ],
+
+  yandexMetrika: {
+    id: '105118320',
+    // debug: process.env.NODE_ENV !== "production",
+    // delay: 0,
+    // cdn: false,
+    // verification: null, // Verification in Yandex Webmaster
+    options: {
+      webvisor: true,
+    },
+  },
 
   vite: {
     build: {
@@ -292,7 +304,7 @@ export default defineNuxtConfig({
     },
   },
 
-  plugins: [{ src: '~/plugins/yandex-metrika.client.ts', mode: 'client' }],
+  // plugins: [{ src: '~/plugins/yandex-metrika.client.ts', mode: 'client' }],
 
   routeRules: {
     '/': { prerender: true },
