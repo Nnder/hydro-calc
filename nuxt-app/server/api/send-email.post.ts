04d0299608090f27f342a2010a06aa38a234fd05
nnder <asfahaikrkt6k56@yandex.ru>
2025-11-08 04:16:06 +0500

    again

--- nuxt-app/server/api/send-email.post.ts
+++ nuxt-app/server/api/send-email.post.ts
@@ -57,7 +57,7 @@ export default defineEventHandler(async event => {
     // Создаем транспортер
     const transporter = nodemailer.createTransport({
       host: 'smtp.yandex.ru',
-      port: 587,
+      port: 465,
       secure: true,
       auth: {
         user: config.smtpUser,
